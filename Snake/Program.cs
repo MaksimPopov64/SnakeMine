cfc8ebf86e9fd48538777d58cf8cf5ba7b58374e
Popetc <maximsc@mail.ru>
2015-11-27 16:52:04 +0300

    Lesson6

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -19,9 +19,24 @@ namespace Snake
            
             p2.Draw();
 
-            
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
 
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
 
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+            numList.RemoveAt(0);
+
+            List<Point> plist = new List<Point>();
+            plist.Add(p1);
+            plist.Add(p2);
 
             Console.ReadLine();
 
