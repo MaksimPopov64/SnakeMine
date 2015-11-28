f5c781f351908518ca9ff0e49acb5ea120f6a11e
Popetc <maximsc@mail.ru>
2015-11-28 17:35:03 +0300

    lesson9

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,20 +10,29 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,2,'$');
-           
-            p1.Draw();
-            
+            Point p1 = new Point(1,2,'$');          
+              
 
             Point p2 = new Point(2,3,'%');
            
-            p2.Draw();
+            
 
-            HorizontalLine line = new HorizontalLine(5,10,8,'+');
+            HorizontalLine line = new HorizontalLine(0,78,0,'+');
+            HorizontalLine line1 = new HorizontalLine(0, 78, 24, '+');        
+
+            VerticalLine line2 = new VerticalLine(0, 24, 0, '+');
+            VerticalLine line3= new VerticalLine(0, 24, 78, '+');
+
+
             line.Drow();
-
-            VerticalLine line1 = new VerticalLine(5, 10, 8, '+');
             line1.Drow();
+            line2.Drow();
+            line3.Drow();
+
+            Point p = new Point(3, 4, '+');
+
+            Snake Snake = new Snake(p, 4, Direction.RIGHT);
+            Snake.Drow();
 
             Console.ReadLine();
 
